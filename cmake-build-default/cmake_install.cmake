5c4f76ddc96178d90948476c18153269272e4033
carra_j <jonathan.carra@epitech.eu>
2017-02-16 12:10:37 +0100

    Mise à jour

--- cmake-build-default/cmake_install.cmake
+++ cmake-build-default/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/jcarra/Ninjaban/21sh
+# Install script for directory: /home/carra_j/rendu/21sh
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -35,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/jcarra/Ninjaban/21sh/cmake-build-default/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/carra_j/rendu/21sh/cmake-build-default/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
