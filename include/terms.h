97ce1877a2955dedf6193a5955b51e57c61ba63f
carra_j <jonathan.carra@epitech.eu>
2017-03-07 00:26:22 +0100

    keymap prepa

--- include/terms.h
+++ include/terms.h
@@ -19,6 +19,9 @@
 # define KEY_BAC 127
 # define KEY_DEL 2117294875
 
+# define KEY_CPY
+# define KEY_PST
+
 # define KEY_UPS 4283163
 # define KEY_DOW 4348699
 # define KEY_RIG 4414235
@@ -31,6 +34,20 @@
 # include <curses.h>
 # include <term.h>
 # include <sys/ioctl.h>
+
+/*
+** Todo : 1 fonction keymap init qui initialise les keymap par default
+**
+** Todo : 1 fonction keynap qui affiche des controle actuel.
+**
+** Todo : 1 fonction keymap --set qui prend en param 1 action sous forme de
+** macro et qui demande a l'utilisateur le controle a associer, verifie si il
+** n'est pas déjà utilisé, et l'associe dans ce cas, sinon, redemande un
+** nouveau controle.
+**
+** Todo : 1 fonction keymap --reset qui remet les param des keymap par défault
+** (voir les macro au dessus)
+*/
 
 int					fputchar(int c);
 int					ft_goto(char *cmd, int col, int line);
